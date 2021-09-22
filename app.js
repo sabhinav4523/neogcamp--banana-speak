af5ec2816e41ba9b0af93017756a97670c58287d
ABHINAV KUMAR SINGH <b216049@iiit-bh.ac.in>
2021-09-22 10:49:49 +0530

    import javascript

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("script is working");
+
+//input
+var userName =prompt("Give me your user name")
+
+//processing
+var welcomeMessage= "this script works"+ userName;
+//output
+alert(welcomeMessage);
